be87ea49c67998b6e6e91a12e2a92db8ba164c2c
Ансар Бектимиров <ansarbektimirov@MacBook-Air-Ansar.local>
2025-03-28 16:29:06 +0600

    priceiconsUIwidthSizedBox

--- test/ListCreator_test.dart
+++ test/ListCreator_test.dart
@@ -1,13 +1,36 @@
-import 'package:coffeeshop/widgets/ListCreator.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 
 
 void main() {
   test('updateSelectedCategory is updates this category', () {
-    //arrang
+    //arrange
+    final List<String> categories = [
+    'Кофе с молоком', 'Черный кофе', 'Колд брю', 'Горячий шоколад', 'Чай'
+  ];
+  int selectedIndex = 0;
     //act
+    int updateSelectedCategory(String categoryName) {
+    final index = categories.indexOf(categoryName);
+    if (index != -1 && index != selectedIndex) {
+      selectedIndex = index;
+      return index;
+    }
+    else {
+      return -1;
+    }
+  }
     //assert
+    expect(updateSelectedCategory('Чай'),4);
     
+  });
+
+
+
+  test("", () {
+
+
+
+
   });
 }
