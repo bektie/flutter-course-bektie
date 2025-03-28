ff061cd0ab674f286ab97726e9df4f1f77525760
Ансар Бектимиров <ansarbektimirov@MacBook-Air-Ansar.local>
2025-03-28 15:26:53 +0600

    fixed UI for priceicons

--- test/ListCreator_test.dart
+++ test/ListCreator_test.dart
@@ -2,10 +2,12 @@ import 'package:coffeeshop/widgets/ListCreator.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 
+
 void main() {
-  test('updateSelectedCategory is updates this category', () async {
-    final ListCreatorState listCreatorState = ListCreatorState();
-    listCreatorState.updateSelectedCategory('Кофе с молоком');
-    expect(listCreatorState.selectedIndex, 0);
+  test('updateSelectedCategory is updates this category', () {
+    //arrang
+    //act
+    //assert
+    
   });
 }
