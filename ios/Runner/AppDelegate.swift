01c7962efa1a08bebcf50f798678282acc23bc90
Ансар Бектимиров <ansarbektimirov@MacBook-Air-Ansar.local>
2025-05-08 23:24:15 +0500

    used to ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,16 @@
 import Flutter
 import UIKit
 
+import YandexMapsMobile
+
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Your preferred language. Not required, defaults to system language
+    YMKMapKit.setApiKey("740472b9-1c74-4f14-9c10-678b49452468") // Your generated API key
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
